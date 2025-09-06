2dc9cbc41dc898521a0b27c31016bcddcae8c29c
unknown <tamgan777@gmail.com>
2025-09-06 14:58:59 +0530

    operator

--- operators.js
+++ operators.js
@@ -17,3 +17,40 @@ console.log("The value of x%y is ",x%y);
 
 // exponentiation
 console.log("The value of x**y is ",x**y);
+//increment
+console.log("The value of ++x is ",++x);
+console.log("The value of x++ is ",x++);
+console.log("The value of x after x++ is ",x);
+let a=10,b="10";
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a<=b);
+console.log(a>=b);
+console.log(a>b);   
+console.log(a<b);
+let age=19;
+console.log(age>18 && age<21);
+console.log(age>18 || age<21);
+console.log(!(age>18 && age<21));
+let firstName="git";
+let lastName="baba";
+let fullName=firstName+lastName;
+console.log(fullName);
+console.log( firstName+" "+lastName);
+let correctName=`hello ${firstName} ${lastName}`;
+console.log(correctName);
+if(age>18){
+    console.log("You can drive");
+}
+else{
+    console.log("You cannot drive");
+}let result=age>18?"You can drive":"You cannot drive";
+console.log(result);
+let count  =101;
+console.log(count++);
+console.log(++count);
+console.log(--count);
+console.log(count--);
+console.log(typeof count);
+
