7088f72c2f3986af1239ef6caa9c46b8bfd998e3
unknown <tamgan777@gmail.com>
2025-09-06 14:31:23 +0530

    operator

---
+++ operators.js
@@ -0,0 +1,19 @@
+let x=10,y=3;
+let add,sub,div,multi,mod,exp;
+// addition 
+console.log("The value of x+y is ",x+y);
+
+// subtraction
+console.log("The value of x-y is ",x-y);
+
+// multiplication       
+console.log("The value of x*y is ",x*y);
+
+// division
+console.log("The value of x/y is ",x/y);
+
+// modulus
+console.log("The value of x%y is ",x%y);
+
+// exponentiation
+console.log("The value of x**y is ",x**y);
