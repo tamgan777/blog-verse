61b40e36933dad6cc332b7972469ce49ff13b276
unknown <tamgan777@gmail.com>
2025-09-06 15:57:15 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,25 @@
+function greet()
+{
+    console.log("print hi");
+}
+greet();
+function add(a,b)
+{
+    return a+b;
+}
+let result =add(5,10);
+console.log(result);
+const pi=3.14;
+console.log(pi);
+let multiply=function(a,b)
+{
+    return a*b;
+}
+console.log(multiply(5,10));
+const square=(x)=>x*x;
+console.log(square(5));
+const subtract =(a,b)=>a-b;
+console.log(subtract(10,5));
+
+
+
